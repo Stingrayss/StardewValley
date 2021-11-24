02fb8cd2448fe0a4816ca49519a893f8f41d054d
Cade Duncan <cadeduncan72@gmail.com>
2021-11-23 23:48:31 -0800

    My Mods

---
+++ CommunityCenterAnywhere/CommunityCenterAnywhere/CommunityCenterAnywhere/CCA.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using Microsoft.Xna.Framework;
+
+namespace CommunityCenterAnywhere
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Display.MenuChanged += this.OnMenuUpdate;
+        }
+        private void OnMenuUpdate(object sender, MenuChangedEventArgs e)
+        {
+            if (!Context.IsWorldReady) // quarter-second
+                return;
+
+            if (Game1.activeClickableMenu is JunimoNoteMenu menu)
+            {
+                foreach (Bundle bundle in menu.bundles)
+                {
+                    if (bundle.name is "2,500g" || bundle.name is "5,000g" || bundle.name is "10,000g" || bundle.name is "25,000g")
+                    {
+                        menu.purchaseButton = new ClickableTextureComponent(new Rectangle(menu.xPositionOnScreen + 800, menu.yPositionOnScreen + 504, 260, 72), menu.noteTexture, new Rectangle(517, 286, 65, 20), 4f);
+                    }
+                    bundle.depositsAllowed = true;
+                }
+            }
+        }
+    }
+}
---
+++ TelephonePurchasing/TelephonePurchasing/TelephonePurchasing/ModEntry.cs
@@ -0,0 +1,56 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using Microsoft.Xna.Framework;
+
+namespace StoresAnywhere
+{
+    public class ModEntry : Mod
+    { 
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Display.MenuChanged += this.updatePhone;
+        }
+
+        private Vector2 playerTile;
+        private GameLocation playerLocation;
+        private void updatePhone(object sender, MenuChangedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+            if (e.OldMenu is CarpenterMenu || e.OldMenu is PurchaseAnimalsMenu)
+            {
+                Game1.warpFarmer(playerLocation.name, (int)playerTile.X, (int)playerTile.Y, Game1.player.facingDirection);
+            }
+            if (!(Game1.activeClickableMenu is PurchaseAnimalsMenu) && !(Game1.activeClickableMenu is CarpenterMenu) && !(Game1.activeClickableMenu is ShopMenu))
+            {
+                return;
+            }
+            if (Game1.activeClickableMenu is ShopMenu menu)
+            {
+                menu.readOnly = false;
+                return;
+            }
+
+            playerTile = Game1.player.getTileLocation();
+            playerLocation = Game1.player.currentLocation;
+
+            if (Game1.activeClickableMenu is PurchaseAnimalsMenu aMenu)
+            {
+                aMenu.readOnly = false;
+            }
+            if (Game1.activeClickableMenu is CarpenterMenu cMenu)
+            {
+                cMenu.readOnly = false;
+                cMenu.upgradeIcon.visible = true;
+                cMenu.demolishButton.visible = true;
+                cMenu.moveButton.visible = true;
+                cMenu.okButton.visible = true;
+                cMenu.paintButton.visible = true;
+            }
+        }
+    }
+}
