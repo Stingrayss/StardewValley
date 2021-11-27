061696ecb415307d8c75a66e2793cc07e4d49769
Cade Duncan <cadeduncan72@gmail.com>
2021-11-26 16:09:14 -0800

    Fixed softlocking bug + added config file

---
+++ CommunityCenterAnywhere/ModConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace CommunityCenterAnywhere
+{
+    public class ModConfig
+    {
+        public bool EnableMod { get; set; } = true;
+        public bool PlayerWarping { get; set; } = true;
+    }
+}
--- CommunityCenterAnywhere/ModEntry.cs
+++ CommunityCenterAnywhere/ModEntry.cs
@@ -8,17 +8,41 @@ namespace CommunityCenterAnywhere
 {
     public class ModEntry : Mod
     {
+
+        public static ModConfig Config;
         public override void Entry(IModHelper helper)
         {
+            Config = Helper.ReadConfig<ModConfig>();
+            if (!Config.EnableMod)
+                return;
             helper.Events.Display.MenuChanged += OnMenuUpdate;
         }
+
+        private Vector2 playerTile;
+        private GameLocation playerLocation;
         private void OnMenuUpdate(object sender, MenuChangedEventArgs e)
         {
+
             if (!Context.IsWorldReady)
                 return;
 
+            //warp the player back to their original location when they are done
+            if (e.OldMenu is JunimoNoteMenu && (!(e.NewMenu is JunimoNoteMenu)) && (!(playerLocation.name.Value is "CommunityCenter")) && Config.PlayerWarping)
+                Game1.warpFarmer(playerLocation.name, (int)playerTile.X, (int)playerTile.Y, Game1.player.facingDirection);
+
             if (e.NewMenu is JunimoNoteMenu menu)
             {
+                //update player location only if they have not previously been in the Community Center menu
+                if (!(e.OldMenu is JunimoNoteMenu))
+                {
+                    playerTile = Game1.player.getTileLocation();
+                    playerLocation = Game1.player.currentLocation;
+                }
+
+                //warp the player to the Community Center so that bundle completion will be counted
+                if (!(Game1.player.currentLocation.name.Value is "CommunityCenter") && Config.PlayerWarping)
+                    Game1.warpFarmer(Game1.getLocationFromName("CommunityCenter").name, 32, 18, Game1.player.facingDirection);
+
                 foreach (Bundle bundle in menu.bundles)
                 {
                     if (bundle.name is "2,500g" || bundle.name is "5,000g" || bundle.name is "10,000g" || bundle.name is "25,000g")
@@ -26,6 +50,7 @@ namespace CommunityCenterAnywhere
 
                     bundle.depositsAllowed = true;
                 }
+
             }
         }
     }
